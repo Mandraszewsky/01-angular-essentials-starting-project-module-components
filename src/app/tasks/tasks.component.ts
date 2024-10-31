56e94708564e36e00c768b1eb7ffafbf680376d8
Mandraszewsky <mandrah@o2.pl>
2024-10-31 15:07:52 +0100

    first service (taskService) with dependency injection

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input } from '@angular/core';
+
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from "./new-task/new-task.component";
 import { NewTaskData } from './task/task.model';
+import { TaskService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -16,38 +18,16 @@ export class TasksComponent {
 
   isAddingTask = false;
 
-  dummyTasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ];
+  // private taskService = new TaskService(); <-- initiate new class
+  // instead of initiate new class (each component will have separate instance of this class), better use dependency injection for one shared repository beetwen components:
+  constructor(private taskService: TaskService) {}
 
   get selectedUserTasks() {
-    return this.dummyTasks.filter((task) => task.userId === this.userId);
+    return this.taskService.getUserTasks(this.userId);
   }
 
   onCompleteTask(id: string) {
-    this.dummyTasks = this.dummyTasks.filter((task) => task.id !== id);
+
   }
 
   onStartAddTask() {
@@ -59,14 +39,6 @@ export class TasksComponent {
   }
 
   onAddTask(taskData: NewTaskData) {
-    this.dummyTasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date,
-    });
-    
     this.isAddingTask = false;
   }
 
